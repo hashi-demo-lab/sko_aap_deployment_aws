748cbc9591c3ad2963935b106757b177b6169b08
Tony Phan <tphan@hashicorp.com>
2025-01-29 16:07:39 +1100

    Remove validation rules and default value for deployment_id variable in variables.tf

--- 3-deploy_aap_aws/variables.tf
+++ 3-deploy_aap_aws/variables.tf
@@ -1,11 +1,6 @@
 variable "deployment_id" {
   description = "Creates a random string that will be used in tagging for correlating the resources used with a deployment of AAP."
   type = string
-  validation {
-    condition = ((length(var.deployment_id) >= 2 && length(var.deployment_id)<=10) || length(var.deployment_id) == 0) && (can(regex("^[a-z]", var.deployment_id)) || var.deployment_id == "")
-    error_message = "deployment_id length should be between 2-10 chars and should contain lower case alpha chars only"
-  }
-  default = "skoaap"
 }
 
 variable "aws_vpc_cidr" {
