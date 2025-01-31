71b890aacb2abf382e94544dd3048519955c6a56
Tony Phan <tphan@hashicorp.com>
2025-01-31 20:48:21 +1100

    Add userpass authentication and policies for organizations in Vault configuration

--- 4-vault-config-ssh/auth.tf
+++ 4-vault-config-ssh/auth.tf
@@ -1,3 +1,21 @@
 resource "vault_auth_backend" "userpass" {
   type = "userpass"
 }
+
+resource "vault_generic_endpoint" "userpass" {
+  for_each = var.oraganizations
+
+  path                 = "auth/userpass/users/${each.key}"
+  ignore_absent_fields = true
+
+  data_json = <<EOT
+{
+  "policies": ["${each.key}"],
+  "password": "Hashicorp123!"
+}
+EOT
+
+  depends_on = [
+    vault_auth_backend.userpass
+  ]
+}
--- 4-vault-config-ssh/policies.tf
+++ 4-vault-config-ssh/policies.tf
@@ -1,11 +1,11 @@
-# resource "vault_policy" "oraganizations" {
-#   for_each = var.oraganizations
+resource "vault_policy" "oraganizations" {
+  for_each = var.oraganizations
 
-#   name = each.key
+  name = each.key
 
-#   policy = <<EOT
-# path "${each.key}/sys/policies/*" {
-#    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-# }
-# EOT
-# }
+  policy = <<EOT
+path "${each.key}/sys/policies/*" {
+   capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+EOT
+}
