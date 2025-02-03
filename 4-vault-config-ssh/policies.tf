273474876327fd4493242fb57d488a8d1e19ff41
Tony Phan <tphan@hashicorp.com>
2025-02-03 12:05:36 +1100

    Simplify Vault policy management by allowing all actions in the namespace

--- 4-vault-config-ssh/policies.tf
+++ 4-vault-config-ssh/policies.tf
@@ -4,19 +4,9 @@ resource "vault_policy" "oraganizations" {
   name = each.key
 
   policy = <<EOT
-# Allow policies to be managed in the namespace
-path "${each.key}/sys/policies/*" {
-   capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-}
-
-# Allow cubbyhole to be managed in the namespace
-path "${each.key}/cubbyhole/*" {
-    capabilities = ["create", "read", "update", "delete", "list"]
-}
-
-# Allow key-value secrets engine to be managed in the namespace
-path "${each.key}/secrets/*" {
-    capabilities = ["create", "read", "update", "delete", "list"]
+# Allow verything to be managed in the namespace
+path "${each.key}/*" {
+    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
 }
 EOT
 }
