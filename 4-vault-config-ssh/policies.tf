7a600e48b37a884ae16e880072c35a4f1acfe787
Tony Phan <tphan@hashicorp.com>
2025-02-03 12:00:46 +1100

    Add namespace support for Vault policies and cubbyhole management

--- 4-vault-config-ssh/policies.tf
+++ 4-vault-config-ssh/policies.tf
@@ -4,8 +4,19 @@ resource "vault_policy" "oraganizations" {
   name = each.key
 
   policy = <<EOT
+# Allow policies to be managed in the namespace
 path "${each.key}/sys/policies/*" {
    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+# Allow cubbyhole to be managed in the namespace
+path "${each.key}/cubbyhole/*" {
+    capabilities = ["create", "read", "update", "delete", "list"]
+}
+
+# Allow key-value secrets engine to be managed in the namespace
+path "${each.key}/secrets/*" {
+    capabilities = ["create", "read", "update", "delete", "list"]
 }
 EOT
 }
