42ace54ec02202fef73ffd10d4aa5674d57446d6
Tony Phan <tphan@hashicorp.com>
2025-02-04 15:35:44 +1100

    Add SSH key generation and secret storage for AAP in Vault

--- 4-vault-config-ssh/modules/onboarding-aap-orgs/secrets.tf
+++ 4-vault-config-ssh/modules/onboarding-aap-orgs/secrets.tf
@@ -24,3 +24,17 @@ resource "vault_kv_secret_v2" "sample1" {
 
   namespace = var.namespace
 }
+
+resource "vault_kv_secret_v2" "aap" {
+  mount                      = vault_mount.kvv2.path
+  name                       = "aap"
+  cas                        = 1
+  delete_all_versions        = true
+  data_json                  = jsonencode(
+  {
+    ssh-unsigned-key  = tls_private_key.ssh-key.public_key_openssh,
+  }
+  )
+
+  namespace = var.namespace
+}
