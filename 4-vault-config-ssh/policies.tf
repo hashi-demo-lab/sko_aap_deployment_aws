14d28052c3f6c5112a38ebb4d406d48fe88cefee
Tony Phan <tphan@hashicorp.com>
2025-02-12 19:55:02 +1100

    Fix typo in 'organizations' policy and add read-only policy for Vault

--- 4-vault-config-ssh/policies.tf
+++ 4-vault-config-ssh/policies.tf
@@ -1,12 +1,23 @@
-resource "vault_policy" "oraganizations" {
+resource "vault_policy" "organizations" {
   for_each = var.organizations
 
   name = each.key
 
   policy = <<EOT
-# Allow verything to be managed in the namespace (not recommended for production environments)
+# Allow everything to be managed in the namespace (not recommended for production environments)
 path "${each.key}/*" {
     capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+EOT
+}
+
+resource "vault_policy" "read-only" {
+  name = "read-only"
+
+  policy = <<EOT
+# Allow read-only access to all paths
+path "*" {
+	  capabilities = ["read","list"]
 }
 EOT
 }
